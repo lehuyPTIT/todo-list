5bcfaec8ee2ef0a05988e7be0a85c4cc87412552
lehuy <huyhuys297@gmail.com>
2019-08-16 10:10:55 +0700

    create-react-app , props

---
+++ src/components/TodoItem.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+class TodoItem extends Component {
+    render(){
+        return(
+            <div className='TodoItem'>
+                <p>
+                    {this.props.title}
+                </p>
+                <p>
+                {this.props.title}   
+                </p>
+                <p>
+                {this.props.title}
+                </p>
+            </div>
+        );
+    }
+}
+export default TodoItem;
