3e3151a9b7cb5f037d541cfc96a68f647c5a016b
lehuy <huyhuys297@gmail.com>
2020-02-02 10:02:16 +0700

    demo toto-list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 import TodoItem from './components/TodoItem'
+import checkAll from './components/img/list.svg';
+// import classNames from 'classnames'
+import './App.css';
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      todoItems: [
+        {title:'Mua sữa',isComples:false    },
+        {title:'Mua cà chua',isComples:true},
+        {title:'Mua nước mắm',isComples:true}
+      ]
+    }
+    this.onKeyUp=this.onKeyUp.bind(this);
+    this.onChange=this.onChange.bind(this);
+  }
+  
+  onItemClick(item) {
+    return(event)=>{
+      const isComples=item.isComples;
+      const {todoItems}=this.state;
+      const index=this.state.todoItems.indexOf(item);
+      this.setState({
+        todoItems:[
+          ...todoItems.slice(0,index),
+          {
+            ...item,
+            isComples:!isComples
+          },
+          ...todoItems.slice(index+1)
+        ],
+        newItem:''
+      });
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TodoItem title="helo"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    };
+  };
+  onCheckAll(){
+    
+  }
+  onKeyUp(event){
+    let text=event.target.value;
+    if(event.keyCode===13){
+      if(!text){
+        return;
+      }
+      if(!text.trim()){return};
+      this.setState({
+        todoItems:[
+          {title:text,isComples:false},
+          ...this.state.todoItems
+        ],
+        newItem:''
+
+      });
+    }
+  };
+  onChange(event){
+    this.setState({
+      newItem:event.target.value
+    })
+  }
+  render(){
+    const {todoItems}=this.state;
+    let url=checkAll;
+    return (
+      <div className="App" > 
+
+        <div className='header'>
+          <img src={url} width={32} height={32}/>
+          <input 
+            onKeyUp={this.onKeyUp} 
+            placeholder='Add a new item'
+            value={this.state.newItem}
+            onChange={this.onChange}
+            >
+          </input>
+        </div>
+
+        {
+            todoItems.map((item,index) =>  
+              <TodoItem
+              key={index}  
+              item={item}
+              onClick={this.onItemClick(item)}
+             />)
+        }
+      </div>
+    );   
+  }
+  
 }
 
 export default App;
+
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,18 +1,20 @@
 import React, {Component} from 'react';
+import classNames from 'classnames';
+import logo from './img/check.svg';
+import checkNon from './img/check-non.svg';
 
+import './TodoItem.css';
 
 class TodoItem extends Component {
     render(){
+        let url = logo;
+        const {item,onClick}=this.props;
+        if(item.isComples===false) url=checkNon;
         return(
             <div className='TodoItem'>
-                <p>
-                    {this.props.title}
-                </p>
-                <p>
-                {this.props.title}   
-                </p>
-                <p>
-                {this.props.title}
+                <img onClick={onClick} src={url} width={32} height={32}/>
+                <p className={classNames({'iscomplete':item.isComples})}>  
+                    {item.title}
                 </p>
             </div>
         );
